b15bda5f07d498ac5fa425d90dde4d0f73c36f14
Akazumiii <ramsesletsplay1500@gmail.com>
2024-06-05 16:17:19 +0200

    removed old code

--- login.php
+++ login.php
@@ -27,37 +27,4 @@ if (json_decode($response)->{"status"} == "success") {
 readfile("./index.html");
 echo '<center><p class="warning"> Die eingegebenen Anmeldeinformationen sind falsch. </p></center>';
 }
-
-
-
-/* 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "webtechsteam";
-		
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		if ($conn->connect_error) {
-			die("Verbindung fehlgeschlagen: " . $conn->connect_error);
-		}
-		
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-		
-		//$sql = "SELECT 'username' FROM user WHERE username = '" . $username . "' AND password = '" . $password . "';";
-		$result = file_get_contents('127.0.0.1:5000/login')
-		
-		if (json_decode($result) == success) {
-			session_start();
-			$_SESSION["username"] = $username;
-			header('Location: dashboard.php');
-			//echo "<h1>Erfolgreich eingeloggt</h1>";
-		} else {
-			readfile("./index.html");
-			echo '<center><p class="warning"> Die eingegebenen Anmeldeinformationen sind falsch. </p></center>';
-		}
-		$conn->close();
-		die();
-		
-	} */
 ?>
